79c67902fc5ed761ee902cf5848945ae86d40bd1
Ryan Pavlik <rpavlik@iastate.edu>
2013-04-08 17:36:53 -0500

    add compiler-rt

--- common.sh
+++ common.sh
@@ -14,7 +14,8 @@ echo "Installing to ${INSTALLDIR}"
 GITDIRS=(".:llvm:http://llvm.org/git/llvm.git"
          "llvm/tools:clang:http://llvm.org/git/clang.git"
          "llvm/tools/clang/tools:extra:http://llvm.org/git/clang-tools-extra.git"
-         "llvm/tools/clang/tools:include-what-you-use:git://github.com/vancegroup-mirrors/include-what-you-use.git")
+         "llvm/tools/clang/tools:include-what-you-use:git://github.com/vancegroup-mirrors/include-what-you-use.git"
+         "llvm/projects:compiler-rt:https://github.com/llvm-mirror/compiler-rt.git")
 
 # TODO: should have clone commands in here too, with git config branch.master.rebase true
 
