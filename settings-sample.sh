fb824e719278292884150dddd78193c6bc1e302c
Ryan Pavlik <ryan@sensics.com>
2016-08-25 14:15:38 -0500

    Split build script in two, essentially for Ninja support.

--- settings-sample.sh
+++ settings-sample.sh
@@ -9,3 +9,6 @@ export DISABLED_REPOS=(libcxx compiler-rt)
 # Determined using echo | g++ -Wp,-v -x c++ - -fsyntax-only
 # as explained on the libcxx web site.
 export EXTRA_CONFIG="-DLIBCXX_LIBSUPCXX_INCLUDE_PATHS=/usr/include/c++/4.6;/usr/include/c++/4.6/x86_64-linux-gnu;/usr/lib/gcc/x86_64-linux-gnu/4.6/include;/usr/lib/gcc/x86_64-linux-gnu/4.6/include-fixed;/usr/include/x86_64-linux-gnu"
+
+# For the parallel and adventurous - use generic-build instead of build-with-make
+# export EXTRA_CONFIG="-G Ninja"
